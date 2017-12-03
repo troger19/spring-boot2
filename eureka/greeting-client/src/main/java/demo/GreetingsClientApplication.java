884cf1fd4f0987299022392c31c7a7ccd569ad94
jan.babel <jan.babel@emeldi.com>
2017-12-03 12:23:07 +0100

    Eureka Client

--- eureka/greeting-client/src/main/java/demo/GreetingsClientApplication.java
+++ eureka/greeting-client/src/main/java/demo/GreetingsClientApplication.java
@@ -1,85 +1,74 @@
 package demo ;
 
 import com.google.common.util.concurrent.RateLimiter;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.ReflectionUtils;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@EnableCircuitBreaker
-@EnableFeignClients
+//@EnableCircuitBreaker
+//@EnableFeignClients
 @EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GreetingsClientApplication {
 
-    @Bean
-    @LoadBalanced
-    RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+//    @Bean
+//    @LoadBalanced
+//    RestTemplate restTemplate() {
+//        return new RestTemplate();
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(GreetingsClientApplication.class, args);
     }
 }
+// If we want to intercept the response from underlying server and return something else
+//@FeignClient("greetings-service")
+//interface GreetingsClient {
+//
+//    @RequestMapping(method = RequestMethod.GET, value = "/greetings/{name}")
+//    Greeting greet(@PathVariable("name") String name);
+//}
+//
+//@RestController
+//class GreetingsApiGatewayRestController {
+//
+//    private final GreetingsClient greetingsClient;
+//
+//    @Autowired
+//    public GreetingsApiGatewayRestController(GreetingsClient client) {
+//        this.greetingsClient = client;
+//    }
+//
+//    public String fallback(String name) {
+//        return "ohai!";
+//    }
+//
+//    @HystrixCommand(fallbackMethod = "fallback")
+//    @RequestMapping(method = RequestMethod.GET, value = "/hi/{name}")
+//    String greet(@PathVariable String name) {
+//        return this.greetingsClient.greet(name).getGreeting();
+//    }
+//}
+//
+//class Greeting {
+//    private String greeting;
+//
+//    public String getGreeting() {
+//        return greeting;
+//    }
+//}
 
-@FeignClient("greetings-service")
-interface GreetingsClient {
-
-    @RequestMapping(method = RequestMethod.GET, value = "/greetings/{name}")
-    Greeting greet(@PathVariable("name") String name);
-}
-
-@RestController
-class GreetingsApiGatewayRestController {
-
-    private final GreetingsClient greetingsClient;
-
-    @Autowired
-    public GreetingsApiGatewayRestController(GreetingsClient client) {
-        this.greetingsClient = client;
-    }
-
-    public String fallback(String name) {
-        return "ohai!";
-    }
-
-    @HystrixCommand(fallbackMethod = "fallback")
-    @RequestMapping(method = RequestMethod.GET, value = "/hi/{name}")
-    String greet(@PathVariable String name) {
-        return this.greetingsClient.greet(name).getGreeting();
-    }
-}
-
-class Greeting {
-    private String greeting;
-
-    public String getGreeting() {
-        return greeting;
-    }
-}
-
+//Rate limiter - tell load balancer to allow 1 request per 30 seconds
 //@Component
 class RateLimitingZuulFilter extends ZuulFilter {
 
